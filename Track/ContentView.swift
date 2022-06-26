7f07402ef9fcd81ef5a37d3aba05f12d3b76f24e
simransdsu <simran5590@gmail.com>
2022-06-25 22:02:06 -0700

    Initial Commit

---
+++ Track/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Track
+//
+//  Created by Simran Preet Singh Narang on 2022-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Track/TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackApp.swift
+//  Track
+//
+//  Created by Simran Preet Singh Narang on 2022-06-25.
+//
+
+import SwiftUI
+
+@main
+struct TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
